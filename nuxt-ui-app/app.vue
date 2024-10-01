0a98a068b428d4f43f224f49748a2ffb3413551b
EphraimHaber <ephraimxyz@gmail.com>
2024-10-01 16:06:45 +0300

    removed project using @nuxt/ui. saved as zip in case I need it later

--- nuxt-ui-app/app.vue
+++
@@ -1,9 +0,0 @@
-<template :dir="$i18n.localeProperties.dir">
-    <NuxtLayout>
-        <AppHeader />
-        <div style="min-height: 80dvh" class="px-2">
-            <NuxtPage />
-        </div>
-        <AppFooter />
-    </NuxtLayout>
-</template>
--- nuxt-ui-app/components/AppFooter.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-    <div style="border: 1px solid red" class="flex justify-between p-5">Component: AppFooter</div>
-</template>
-
-<style scoped></style>
