a0e545e29b7e1237d5f8e0c3fc2e36f88018d7be
EphraimHaber <ephraimxyz@gmail.com>
2024-10-01 18:41:27 +0300

    fucking ez

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <NuxtLayout>
+        <AppHeader />
+        <v-app>
+            <NuxtPage />
+        </v-app>
+        <AppFooter />
+    </NuxtLayout>
 </template>
---
+++ frontend/components/AppHeader.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>{{ $t('app_title') }}</div>
+</template>
+
+<style scoped></style>
